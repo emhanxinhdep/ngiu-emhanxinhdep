cc0090bf1219450c9ddf97638d457d3e0525a8ca
emhanxinhdep <106363138+emhanxinhdep@users.noreply.github.com>
2022-05-27 16:59:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    introTitle: 'Gud morrninggg, I luv u!',
+    introDesc: `Anh iu ahh...
+    bé triet ahhh...
+    bé han có điều này muốn hỏi nhớ phải trả lời thật nòng nhaa`,
+    btnIntro: 'ố kê lunn',
+    title: 'Anh có iu em hongg??',
+    desc: 'Nếu một mai anh có đi thật xa. Thì tình yêu của em sẽ đưa anh về nhà',
+    btnYes: 'Có',
+    btnNo: 'Có Có Có :3',
+    question:'Trả lời cộc lốc z đó hả! Thế nói xem tại sao lại iu em za, nói thật ii <3',
+    btnReply: 'Gửi cho bé han <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Ỏooo sự đáng yêu của anh đã giết chết em 5 lần. penta cute! ',
+    messDesc: 'bé triet nhắn tin cho iem ngay đi nhóo',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://www.facebook.com/messages/t/100024550323193'
